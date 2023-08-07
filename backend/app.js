355f484977d49315aa2c878bec1c4e550f32636e
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-08-07 19:37:56 +0300

    v4

--- backend/app.js
+++ backend/app.js
@@ -14,14 +14,18 @@ const routesUsers = require('./routes/users');
 const routesCards = require('./routes/cards');
 const routesNotFound = require('./routes/errorNotFound');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 mongoose.connect(DB_URL);
 
 app.use(cors({
   origin: [
-    'http://localhost:3001',
+    'http://localhost:3000',
+    'https://lindasux.students.nomoreparties.co',
+    'http://lindasux.students.nomoreparties.co',
+    'https://api.lindasux.students.nomoreparties.co',
+    'http://api.lindasux.students.nomoreparties.co',
   ],
 }));
 
