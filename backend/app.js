23ec878ac2b694d84c4f522af923d73fbe95b751
Linda Suhovenko <suhovenko_linda@mail.ru>
2023-08-08 08:54:06 +0300

    v4

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,8 @@ mongoose.connect(DB_URL);
 
 app.use(cors({
   origin: [
-    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://lindasux.students.nomoreparties.co',
   ],
 }));
 
